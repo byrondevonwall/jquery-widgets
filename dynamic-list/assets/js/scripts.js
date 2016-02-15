b58801c3ff7f6af295b1a0d4642909b425732d04
Byron Wall <byrondevonwall@gmail.com>
2016-02-15 17:57:04 -0500

    Add a lil more functionality to dynamic list

--- dynamic-list/assets/js/scripts.js
+++ dynamic-list/assets/js/scripts.js
@@ -1,9 +1,20 @@
 $(function(){
   'use strict';
 
-$(this).keyup(function(){
-  if ($(this).text.length >0){
-    $("#list").append("<br><input type='text' value'>");
+
+$(".box1").keypress(function(){
+
+  if( $(".box1").val() <= 1){
+    $("#list").append("<br><input type='text' value=''>");
   }
+    console.log($(".box1").val())
 });
+
+// $(this).keydown(function(){
+//   console.log(this)
+//   if($(this).text.length > 0 && $(this).text.length <= 1){
+//     $("#list").append("<br><input type='text' value=''>");
+//   }
+// });
+
 });
